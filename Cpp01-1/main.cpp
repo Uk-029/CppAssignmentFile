c36a73e51c0e1c2b97bb754c311cf0fbdb81a88f
Uk-029 <157310062+Uk-029@users.noreply.github.com>
2024-07-12 02:47:24 +0900

    Add files via upload

---
+++ Cpp01-1/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "header1.h"
+#include "header2.h"
+
+using namespace header1;
+using namespace std;
+
+int main() {
+	cout << "Hello, World!!!" << endl;
+	return 0;
+
+	foo();				//header1
+	bar();				//header1
+	header2::foo();		//header2
+	header2::bar();		//header2
+}
